077fab0446b9706b542f78d5d9869581ba272dd0
Alex Kovacs <alexkovacs@rocketmail.com>
2022-11-07 22:37:46 +0200

    Finish the project

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ function flip(elem, first) {
   const deltaX = first.boundingRect.left - last.left;
   const deltaY = first.boundingRect.top - last.top;
 
-
   //   play
   elem.animate(
     [
@@ -36,11 +35,11 @@ function animateHeader() {
   //first
   const logoFirst = {
     boundingRect: logo$.getBoundingClientRect(),
-    offset: (logo$.offsetHeight - logo$.clientHeight) / 2
+    offset: (logo$.offsetHeight - logo$.clientHeight) / 2,
   };
   const searchBarFirst = {
     boundingRect: searchBar$.getBoundingClientRect(),
-    offset: (searchBar$.offsetHeight - searchBar$.clientHeight) / 2
+    offset: (searchBar$.offsetHeight - searchBar$.clientHeight) / 2,
   };
 
   //last
@@ -52,31 +51,36 @@ function animateHeader() {
 }
 
 const urls = Object.freeze({
-  1: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
-  2: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=',
-  3: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=',
+  1: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=",
+  2: "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=",
+  3: "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=",
 });
 
 const favorites = new Map(JSON.parse(localStorage.getItem("favorites")));
 
 function saveFavorites() {
-  localStorage.setItem('favorites', JSON.stringify(Array.from(favorites.entries())));
+  localStorage.setItem(
+    "favorites",
+    JSON.stringify(Array.from(favorites.entries()))
+  );
 }
 
 function clearFavorites() {
   favorites.clear();
   saveFavorites();
 
-  const children = searchResults$.querySelector("#result-list").children
-  for(let child of children) {
-    child.classList.remove('favorite');
+  const children = favoritesPage$.querySelector("#favorites-list").children;
+  for (let child of children) {
+    child.classList.remove("favorite");
   }
-  detailsContent$.querySelector('#toggle-favorite-btn').classList.remove('favorite');
+  favoritesDetailsContent$
+    .querySelector("#toggle-favorite-btn")
+    .classList.remove("favorite");
 }
 
 function toggleFavorite(drink, toUpdate) {
   let isFavorite;
-  if(favorites.has(drink.id)) {
+  if (favorites.has(drink.id)) {
     favorites.delete(drink.id);
     isFavorite = false;
   } else {
@@ -85,66 +89,80 @@ function toggleFavorite(drink, toUpdate) {
   }
 
   saveFavorites();
-  
-  toUpdate.forEach(el$ => el$.classList.toggle('favorite', isFavorite));
+
+  toUpdate.forEach((el$) => el$.classList.toggle("favorite", isFavorite));
 }
 
-function closeDetails() {
-  detailsContent$.classList.remove('active');
+function removeFavorite(drink) {
+  if (!favorites.has(drink.id)) return;
+
+  favorites.delete(drink.id);
+  saveFavorites();
+}
+
+function closeDetails(container$) {
+  container$.classList.remove("active");
 }
 
 function createIngredientItem(ingredient, drink) {
-  const li$ = document.createElement('li');
-  li$.classList.add('ingredient');
-  
+  const li$ = document.createElement("li");
+  li$.classList.add("ingredient");
+
   li$.innerHTML = `
     <h2>${ingredient.name}</h2>
-    <p class="center ${ingredient.measure == null ? 'not-specified' : ''}" >${ingredient.measure ?? 'Not specified'}</p>
+    <p class="center ${ingredient.measure == null ? "not-specified" : ""}" >${
+    ingredient.measure ?? "Not specified"
+  }</p>
   `;
 
-  const deleteBtn$ = document.createElement('button');
-  deleteBtn$.classList.add('delete-btn', 'btn');
+  const deleteBtn$ = document.createElement("button");
+  deleteBtn$.classList.add("delete-btn", "btn");
   deleteBtn$.innerHTML = '<i class="fa-solid fa-trash"></i>';
-  deleteBtn$.onclick = _ => { 
-    drink.ingredients = drink.ingredients.filter(x => x.name != ingredient.name);
+  deleteBtn$.onclick = (_) => {
+    drink.ingredients = drink.ingredients.filter(
+      (x) => x.name != ingredient.name
+    );
     li$.remove();
-  }
+  };
 
   li$.appendChild(deleteBtn$);
   return li$;
 }
 
 function getCreateIngredientTemplate(ingredientList$, drink) {
-  const li$ = document.createElement('li');
-  li$.classList.add('ingredient', 'ingredient-add');
+  const li$ = document.createElement("li");
+  li$.classList.add("ingredient", "ingredient-add");
 
-  const nameInput$ = document.createElement('input');
-  const measureInput$ = document.createElement('input');
+  const nameInput$ = document.createElement("input");
+  const measureInput$ = document.createElement("input");
 
   li$.appendChild(nameInput$);
   li$.appendChild(measureInput$);
 
-  const errMessage$ = createIngredientItem({name: "", measure: "* The ingridient name cannot be null."});
+  const errMessage$ = createIngredientItem({
+    name: "",
+    measure: "* The ingridient name cannot be null.",
+  });
   errMessage$.lastChild.remove();
-  errMessage$.style.display = 'none';
-  errMessage$.classList.add('favorite');
+  errMessage$.style.display = "none";
+  errMessage$.classList.add("favorite");
 
-  const addBtn$ = document.createElement('button');
-  addBtn$.classList.add('add-btn', 'btn');
+  const addBtn$ = document.createElement("button");
+  addBtn$.classList.add("add-btn", "btn");
   addBtn$.innerHTML = '<i class="fa-solid fa-square-plus"></i>';
-  addBtn$.onclick = _ => {
+  addBtn$.onclick = (_) => {
     const name = nameInput$.value;
-    if(!name) return errMessage$.style.display = 'flex';
-    errMessage$.style.display = 'none';
+    if (!name) return (errMessage$.style.display = "flex");
+    errMessage$.style.display = "none";
     const ingredient = {
       name,
-      measure: (!measureInput$.value) ? null : measureInput$.value,
+      measure: !measureInput$.value ? null : measureInput$.value,
     };
     ingredientList$.insertBefore(createIngredientItem(ingredient, drink), li$);
-    nameInput$.value = '';
-    measureInput$.value = '';
+    nameInput$.value = "";
+    measureInput$.value = "";
     drink.ingredients.push(ingredient);
-  }
+  };
 
   li$.appendChild(addBtn$);
 
@@ -152,32 +170,44 @@ function getCreateIngredientTemplate(ingredientList$, drink) {
   ingredientList$.appendChild(errMessage$);
 }
 
-function showDetails(drink, source$) {
-  detailsContent$.querySelector('.title').innerHTML = `${drink.name} (${drink.id})`;
-  detailsContent$.querySelector('img').src = drink.imgUrl;
+function showDetails(container$, drink, source$) {
+  container$.querySelector(".title").innerHTML = `${drink.name} (${drink.id})`;
+  container$.querySelector("img").src = drink.imgUrl;
 
-  details$.querySelector('#remove-drink-btn').onclick = _ => alert('to implement')
+  container$.parentElement.querySelector("#remove-drink-btn").onclick = (_) => {
+    deleteDialog$.querySelector(".cancel").onclick = (_) =>
+      deleteDialog$.close();
+    deleteDialog$.querySelector(".delete").onclick = (_) => {
+      source$.remove();
+      closeDetails(container$);
+      removeFavorite(drink);
+      deleteDialog$.close();
+    };
+    deleteDialog$.showModal();
+    // alert("to implement");
+  };
 
-  const toggleFavoriteBtn$ = detailsContent$.querySelector('#toggle-favorite-btn');
-  toggleFavoriteBtn$.onclick = _ => toggleFavorite(drink, [source$, toggleFavoriteBtn$]);
-  toggleFavoriteBtn$.classList.toggle('favorite', favorites.has(drink.id))
+  const toggleFavoriteBtn$ = container$.querySelector("#toggle-favorite-btn");
+  toggleFavoriteBtn$.onclick = (_) =>
+    toggleFavorite(drink, [source$, toggleFavoriteBtn$]);
+  toggleFavoriteBtn$.classList.toggle("favorite", favorites.has(drink.id));
 
-  const ingredientList$ = detailsContent$.querySelector('.section-content');
-  ingredientList$.innerHTML = '';
-  drink.ingredients.forEach(ingredient => {
+  const ingredientList$ = container$.querySelector(".section-content");
+  ingredientList$.innerHTML = "";
+  drink.ingredients.forEach((ingredient) => {
     const ingredient$ = createIngredientItem(ingredient, drink);
     ingredientList$.appendChild(ingredient$);
   });
 
-  getCreateIngredientTemplate(ingredientList$, drink)
+  getCreateIngredientTemplate(ingredientList$, drink);
 
-  detailsContent$.classList.add('active');
+  container$.classList.add("active");
 }
 
-function createResultItem(drink) {
-  const li$ = document.createElement('li');
-  li$.classList.add('result');
-  li$.classList.toggle('favorite', favorites.has(drink.id))
+function createResultItem(container$, drink) {
+  const li$ = document.createElement("li");
+  li$.classList.add("result");
+  li$.classList.toggle("favorite", favorites.has(drink.id));
   li$.innerHTML = `
     <div class="color-bar"></div>
     <div class="result-content">
@@ -195,20 +225,17 @@ function createResultItem(drink) {
       </div>
     </div>
   `;
-  li$.onclick = _ => showDetails(drink, li$);
+  li$.onclick = (_) => showDetails(container$, drink, li$);
   return li$;
 }
 
 function addResults(drinks) {
   const resultList$ = searchResults$.querySelector("#result-list");
-  resultList$.innerHTML = '';
+  resultList$.querySelectorAll("li").forEach((el) => el.remove());
 
-  if(drinks.length == 0) return searchResults$.querySelector('.empty').style.display = 'flex';
-  else searchResults$.querySelector('.empty').style.display = 'none';
-
-  drinks.forEach(drink => {
-    const result = createResultItem(drink);
-    resultList$.appendChild(result);
+  drinks.forEach((drink) => {
+    const result = createResultItem(searchDetailsContent$, drink);
+    resultList$.insertBefore(result, resultList$.lastElementChild);
   });
 }
 
@@ -216,54 +243,67 @@ async function search() {
   const container$ = main$.querySelector("#search-results .results");
 
   // start spinner
-  container$.classList.remove('active');
+  container$.classList.remove("active");
 
-  const filter = searchBar$.querySelector('.search-filter').value;
+  const filter = searchBar$.querySelector(".search-filter").value;
   const url = urls[filter];
 
-  const query = searchBar$.querySelector('.search-input').value;
+  const query = searchBar$.querySelector(".search-input").value;
 
   let drinks;
   try {
-    const res = await fetch(`${url}${query}`)
-    const data = await res.json()
-    drinks = data.drinks?.map(drink => {
+    const res = await fetch(`${url}${query}`);
+    const data = await res.json();
+    drinks = data.drinks?.map((drink) => {
       const ingredients = [];
-      for(let i = 1; i <= 15; i++) {
-        if(drink[`strIngredient${i}`] == null) break;
+      for (let i = 1; i <= 15; i++) {
+        if (drink[`strIngredient${i}`] == null) break;
         ingredients.push({
           name: drink[`strIngredient${i}`],
-          measure: drink[`strMeasure${i}`]
-        })
+          measure: drink[`strMeasure${i}`],
+        });
       }
-  
+
       return {
         id: drink.idDrink,
         name: drink.strDrink,
         category: drink.strCategory,
         imgUrl: drink.strDrinkThumb,
-        ingredients
+        ingredients,
       };
     });
 
-    searchResults$.querySelector('.empty').innerHTML = 'Nothing found!'
+    searchResults$.querySelector(".empty").innerHTML = "Nothing found!";
   } catch (err) {
-    searchResults$.querySelector('.empty').innerHTML = 'Oops! Something went wrong... Please try again.'
+    searchResults$.querySelector(".empty").innerHTML =
+      "Oops! Something went wrong... Please try again.";
   }
 
   addResults(drinks ?? []);
 
   // stop spinner
-  container$.classList.add('active');
+  container$.classList.add("active");
+}
+
+function startSearch() {
+  navigate(true);
+  closeDetails(searchDetailsContent$);
+  if (body$.classList.contains("init")) {
+    animateHeader();
+    return setTimeout(search, 500);
+  }
+
+  search();
 }
 
 const body$ = document.querySelector("body");
 const main$ = body$.querySelector("main");
 
-const searchResults$ = main$.querySelector('#search-results');
-const details$ = searchResults$.querySelector('.details');
-const detailsContent$ = details$.querySelector('#content');
-detailsContent$.querySelector('#close-content-btn').onclick = closeDetails
+const searchResults$ = main$.querySelector("#search-results");
+const searchDetails$ = searchResults$.querySelector(".details");
+const searchDetailsContent$ = searchDetails$.querySelector("#content");
+searchDetailsContent$.querySelector("#close-content-btn").onclick = (_) =>
+  closeDetails(searchDetailsContent$);
 
 const searchBar$ = document.getElementById("search-bar");
 searchBar$.onkeydown = (e) => {
@@ -271,12 +311,75 @@ searchBar$.onkeydown = (e) => {
 
   if (e.keyCode !== 13) return;
 
-  if(body$.classList.contains('init')) {
-    animateHeader();
-    return setTimeout(search, 500);
-  }
-
-  search();
+  startSearch();
 };
-searchBar$.querySelector('#search-btn').onclick = search
+searchBar$.querySelector("#search-btn").onclick = startSearch;
 searchBar$.querySelector("input").focus();
+
+const nav$ = body$.querySelector("nav");
+const searchBtn$ = nav$.querySelector(".nav-link:first-child");
+const favoriteBtn$ = nav$.querySelector(".nav-link:last-child");
+
+function navigate(toSearch) {
+  favoriteBtn$.classList.toggle("active", !toSearch);
+  searchBtn$.classList.toggle("active", toSearch);
+
+  const searchPage$ = main$.querySelector("#search-results");
+  const favoritePage$ = main$.querySelector("#favorites-page");
+
+  favoritePage$.classList.toggle("active", !toSearch);
+  searchPage$.classList.toggle("active", toSearch);
+}
+
+const navigateToSearch = (_) => startSearch();
+const navigateToFavorites = (_) => {
+  const container$ = favoritesPage$.querySelector(".favorites");
+
+  // start spinner
+  container$.classList.remove("active");
+
+  navigate(false);
+  closeDetails(favoritesDetailsContent$);
+  if (body$.classList.contains("init")) {
+    animateHeader();
+    return setTimeout((_) => {
+      initFavorites();
+
+      // start spinner
+      container$.classList.add("active");
+    }, 500);
+  }
+  initFavorites();
+
+  // start spinner
+  container$.classList.add("active");
+};
+
+searchBtn$.onclick = (_) => {
+  if (!searchBtn$.classList.contains("active")) navigateToSearch();
+};
+favoriteBtn$.onclick = navigateToFavorites;
+
+// Favorites
+function initFavorites() {
+  const favoritesList$ = favoritesPage$.querySelector("#favorites-list");
+  favoritesList$.querySelectorAll("li").forEach((el) => el.remove());
+
+  for (let [_, drink] of favorites.entries()) {
+    const result = createResultItem(favoritesDetailsContent$, drink);
+    favoritesList$.insertBefore(result, favoritesList$.lastElementChild);
+  }
+}
+
+const favoritesPage$ = main$.querySelector("#favorites-page");
+const favoritesDetails$ = favoritesPage$.querySelector(".details");
+const favoritesDetailsContent$ = favoritesDetails$.querySelector("#content");
+favoritesDetailsContent$.querySelector("#close-content-btn").onclick = (_) =>
+  closeDetails(favoritesDetailsContent$);
+
+favoritesPage$.querySelector("#clear-favorites-btn").onclick = (_) =>
+  clearFavorites();
+
+initFavorites();
+
+const deleteDialog$ = main$.querySelector("#delete-dialog");
